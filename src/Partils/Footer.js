2671b5a858778fb4ab74d7e43b3ae69dc32d7e56
Rohit <rohitkafle360@gmail.com>
2023-10-02 13:50:02 +0530

    change in footer

--- src/Partils/Footer.js
+++ src/Partils/Footer.js
@@ -1,21 +1,59 @@
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-export default function Footer(){
+export default function Footer() {
+  return (
+    <>
+      <div className="Footer">
+        <div className="lionimg">
+          <img
+            src="/wallpaperflare.com_wallpaper.jpg"
+            alt=""
+            id="right-lion"
+          ></img>
+        </div>
+        <div className="logo">
+          <div className="logos"> 
+            <a
+              href="https://www.linkedin.com/in/rohit-kafle-3b2354260"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p>
+                <FaLinkedin />
+                linkdin
+              </p>{" "}
+            </a>
 
-return(<>
+            <p>
+              <FaInstagram />
+              insta{" "}
+            </p>
 
-<div className="Footer">
-
-<div className="item1"><a href="https://www.linkedin.com/in/rohit-kafle-3b2354260" target="_blank" rel="noopener noreferrer"><p><FaLinkedin/>linkdin</p> </a></div> 
-
-<div className="item1"><p><FaInstagram/>insta </p></div>
-
-<div className="item1"><p><FaFacebook/>facebook</p></div>
-
-</div>
-
-
-</>)
-
-
+            <p>
+              <FaFacebook />
+              facebook
+            </p>
+          </div>
+          <div className="text">
+            <h6>Contact Us</h6>
+            <h6>Our Services</h6>
+            <h6>Privacy Policy</h6>
+            <h6>Terms & Conditions</h6>
+          </div>
+          <div className="copyright">
+                <p>
+                Portfolio Copyright © 2023  All rights reserved || Designed By: Rohit Kafle
+                </p>
+          </div>
+        </div>
+        <div className="lionimg">
+          <img
+            src="/wallpaperflare.com_wallpaper (1).jpg"
+            alt=""
+            id="left-lion"
+          ></img>
+        </div>
+      </div>
+    </>
+  );
 }
